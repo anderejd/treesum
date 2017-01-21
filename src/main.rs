934d11ebf4c0e4d5187eb19ef6414084c07f18fb
anderejd <rajder@gmail.com>
2017-01-21 16:48:33 +0100

    Rerwrite using generics for scatter-gather.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use std::io;
 use walkdir::WalkDir;
 use std::thread;
 use std::sync::Arc;
+use walkdir::DirEntry;
 
 fn calc_hash(p: &Path, hasher: &mut Sha1, buf: &mut [u8]) -> io::Result<String> {
     hasher.reset();
@@ -28,69 +29,61 @@ fn calc_hash(p: &Path, hasher: &mut Sha1, buf: &mut [u8]) -> io::Result<String> 
     Ok(hasher.result_str())
 }
 
-fn do_it(root: &Path) -> io::Result<()> {
-    let jobs = {
-        let (s, r) = chan::sync(0);
-        let pb = root.to_path_buf();
-        thread::spawn(move || {
-            for e in WalkDir::new(pb) {
-                let e = match e {
-                    Ok(e) => e,
-                    Err(_) => continue, //? TODO: send Result with error
-                };
-                if !e.file_type().is_file() {
-                    continue;
-                }
-                s.send(e);
-            }
-        });
-        // This extra lexical scope will drop the initial
-        // sender we created. Thus, the channel will be
-        // closed when all threads spawned above has completed.
-        r
-    };
-    let wg = chan::WaitGroup::new();
-    for _ in 0..8 {
-        wg.add(1);
-        let wg = wg.clone();
-        let jobs = jobs.clone();
-        thread::spawn(move || {
-            let mut hasher = Sha1::new();
-            let mut buf = [0; 1024 * 8];
-            for e in jobs {
-                let hex = match calc_hash(e.path(), &mut hasher, &mut buf) {
-                    Ok(h) => h,
-                    Err(_) => continue, //? TODO: Handle error
-                };
-                println!("{}\t{}", hex, e.path().display());
-            }
-            wg.done();
-        });
+type Result<T> = std::result::Result<T, TreesumError>;
+
+#[derive(Debug)]
+enum TreesumError {
+    Io(io::Error),
+    Ignored(DirEntry),
+    WalkDir(walkdir::Error),
+}
+
+fn process_dir_entry(e: walkdir::Result<DirEntry>) -> Result<String> {
+    let e = e.map_err(TreesumError::WalkDir)?;
+    if !e.file_type().is_file() {
+        return Err(TreesumError::Ignored(e));
     }
-    wg.wait();
+    let mut hasher = Sha1::new();
+    let mut buf = [0; 1024 * 8];
+    calc_hash(e.path(), &mut hasher, &mut buf).map_err(TreesumError::Io)
+}
+
+
+fn do_it_2(root: &Path) -> io::Result<()> {
+    let pb = root.to_path_buf();
+    let iter_factory = || WalkDir::new(pb);
+    let results = scatter_gather(iter_factory, process_dir_entry);
+    for r in results {
+        println!("{:?}", r);
+    }
     Ok(())
 }
 
-fn process_dir_entry(e: walkdir::Result<walkdir::DirEntry>) -> i32 {
-    let e = match e {
-        Ok(e) => e,
-        Err(_) => return 0, //? TODO: send Result with error
-    };
-    if !e.file_type().is_file() {
-        return 0;
+fn main() {
+    let root = env::args().nth(1).unwrap_or(".".to_string());
+    let root = Path::new(root.as_str());
+    do_it_2(root).expect("Hello error 2!");
+}
+
+/// The purpose of this newtype is to hide the channel implementation.
+struct GatherIter<T>(chan::Iter<T>);
+impl<T> Iterator for GatherIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.0.next()
     }
-    1
 }
 
 /// factory is needed to allow iterators without std::marker::Send
 /// TODO: spawn worker threads
 /// TODO: use channels
 /// TODO: return channel Receiver as Iterator
-fn fan_out_in<F, X, I, T>(factory: F, xform: X) -> i32
-    where F: 'static + std::marker::Send + FnOnce() -> T,
-          X: 'static + std::marker::Send + std::marker::Sync + Fn(I) -> i32 + 'static,
-          I: 'static + std::marker::Send,
-          T: IntoIterator<Item = I>
+fn scatter_gather<F, X, J, R, I>(factory: F, xform: X) -> GatherIter<R>
+    where F: 'static + std::marker::Send + FnOnce() -> I,
+          X: 'static + std::marker::Send + std::marker::Sync + Fn(J) -> R,
+          J: 'static + std::marker::Send,
+          R: 'static + std::marker::Send,
+          I: IntoIterator<Item = J>
 {
     let jobs_rx = {
         let (tx, rx) = chan::sync(0);
@@ -103,7 +96,7 @@ fn fan_out_in<F, X, I, T>(factory: F, xform: X) -> i32
     };
     let results_rx = {
         let (tx, rx) = chan::sync(0);
-        let xform = Arc::new(xform);
+        let xform = Arc::new(xform); // TODO: Investigate why this is needed.
         for _ in 0..8 {
             let tx = tx.clone();
             let jobs_rx = jobs_rx.clone();
@@ -116,24 +109,5 @@ fn fan_out_in<F, X, I, T>(factory: F, xform: X) -> i32
         }
         rx
     };
-    let mut i = 0i32;
-    for r in results_rx {
-        i += r;
-    }
-    i
-}
-
-fn do_it_2(root: &Path) -> io::Result<()> {
-    let pb = root.to_path_buf();
-    let iter_factory = || WalkDir::new(pb);
-    let i = fan_out_in(iter_factory, process_dir_entry);
-    println!("{}", i);
-    Ok(())
-}
-
-fn main() {
-    let root = env::args().nth(1).unwrap_or(".".to_string());
-    let root = Path::new(root.as_str());
-    do_it(root).expect("Hello error 1!");
-    do_it_2(root).expect("Hello error 2!");
+    GatherIter(results_rx.iter())
 }
