1c1796f0ffdcc4286a51edb5cf1547f36d24f249
anderejd <rajder@gmail.com>
2017-02-10 22:59:43 +0100

    Cleanup and some initial docs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+/*!
+Treesum calculates check|sums| for files in directory |tree|s. The successful
+results are written to stdout while errors and status messages are written to
+stderr. Example output:
+
+```
+01010101010101010101	root/some_subdir/cat.gif
+12121212121212121212	root/I Like Space In My Paths/HerpDerp Final 3 (17).xlsx
+```
+*/
 #![deny(warnings)]
 
 extern crate crypto;
@@ -14,6 +24,7 @@ use std::path::Path;
 use walkdir::DirEntry;
 use walkdir::WalkDir;
 
+/// Calculate a checksum for a file.
 fn calc_hash(p: &Path, hasher: &mut Sha1, buf: &mut [u8]) -> io::Result<String> {
     hasher.reset();
     let mut f = File::open(p)?;
@@ -27,6 +38,10 @@ fn calc_hash(p: &Path, hasher: &mut Sha1, buf: &mut [u8]) -> io::Result<String> 
     Ok(hasher.result_str())
 }
 
+fn print_success(t: (DirEntry, String)) {
+    println!("{}\t{}", t.1, t.0.path().display());
+}
+
 #[derive(Debug)]
 enum Error {
     Io(io::Error),
@@ -34,8 +49,50 @@ enum Error {
     WalkDir(walkdir::Error),
 }
 
+/// Helper for handling error reporting levels and printing.
+/// TODO: Use stderr. Find crate?
+fn print_err(e: Error, verbose: bool) {
+    match e {
+        Error::Ignored(ent) => {
+            if verbose {
+                println!("ignored: {}", ent.path().display())
+            }
+        }
+        _ => println!("ERROR: {:?}", e),
+    }
+}
+
+type ResultIter = sgiter::GatherIter<Result<(DirEntry, String), Error>>;
+
+/// Allocates memory for and collects all successfull hashes before sorting and
+/// then printing. Errors are printed immidiately.
+fn do_sorted_output(res:ResultIter, verbose:bool) {
+    let mut tuples = vec![];
+    for r in res {
+        match r {
+            Ok(t)  => tuples.push(t),
+            Err(e) => print_err(e, verbose),
+        }
+    }
+    tuples.sort_by(|a, b| a.1.cmp(&b.1));
+    for t in tuples {
+        print_success(t);
+    }
+}
+
+/// Prints both errors and successful hashes immidiately without sorting.
+fn do_unsorted_output(res:ResultIter, verbose:bool) {
+    for r in res {
+        match r {
+            Ok(t)  => print_success(t),
+            Err(e) => print_err(e, verbose),
+        }
+    }
+}
+
+/// Bind constructors for producer and consumers, start scatter gather and
+/// pass on result iterator to handler for either sorted or unsorted output.
 fn process_root(root: &Path) -> io::Result<()> {
-    use sgiter::scatter_gather;
     let pb = root.to_path_buf();
     let producer_ctor = || WalkDir::new(pb);
     let xform_ctor = || {
@@ -52,32 +109,15 @@ fn process_root(root: &Path) -> io::Result<()> {
         };
         f
     };
-    let results = scatter_gather(producer_ctor, xform_ctor);
-    //let sort_on_hash = true;
-    let mut tuples = vec![];
-    for r in results {
-        match r {
-            Ok(t) => tuples.push(t),
-            Err(e) => print_err(e),
-        }
-    }
-    tuples.sort_by(|a, b| a.1.cmp(&b.1));
-    for t in tuples {
-        println!("{}\t{}", t.1, t.0.path().display());
+    let results = sgiter::scatter_gather(producer_ctor, xform_ctor);
+    let verbose = false;       // TODO: add command line flag
+    let sort_successes = true; // TODO: add command line flag
+    if sort_successes {
+        do_sorted_output(results, verbose)
+    } else {
+        do_unsorted_output(results, verbose)
     }
     Ok(())
-}
-
-fn print_err(e: Error) {
-    let verbose = true;
-    match e {
-        Error::Ignored(ent) => {
-            if verbose {
-                println!("ignored: {}", ent.path().display())
-            }
-        }
-        _ => println!("ERROR: {:?}", e),
-    }
 }
 
 fn main() {
@@ -85,3 +125,4 @@ fn main() {
     let root = Path::new(root.as_str());
     process_root(root).unwrap()
 }
+
